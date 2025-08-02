2fb9fb971241e27568749810aa8ca53f3b418c47
conao3 <conao3@gmail.com>
2025-08-02 23:00:53 +0900

    require repl utils

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,7 +1,9 @@
 (ns dev
   (:require
+   [clojure.main :as clojure.main]
    #_{:clj-kondo/ignore [:unused-referred-var]}
    [com.stuartsierra.component.repl :as component.repl :refer [system start stop reset]]
    [mahjong-point-calc.core :as c.core]))
 
 (component.repl/set-init (fn [_] (c.core/new-system)))
+(apply require clojure.main/repl-requires)
