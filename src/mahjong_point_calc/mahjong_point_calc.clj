1a85e7537444440f4443fcf521c49c5a1293e01f
conao3 <conao3@gmail.com>
2025-08-02 23:02:06 +0900

    add serve feature for CLI usage

--- src/mahjong_point_calc/mahjong_point_calc.clj
+++ src/mahjong_point_calc/mahjong_point_calc.clj
@@ -1,8 +1,47 @@
 (ns mahjong-point-calc.mahjong-point-calc
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [clojure.tools.logging :as log]
+   [com.stuartsierra.component :as component]
+   [mahjong-point-calc.core :as c.core]))
+
+(declare commands)
 
 (defn add [a b]
   (+ a b))
 
+(defn eprintln [& args]
+  (binding [*out* *err*]
+    (apply println args)))
+
+(defn cmd-serve
+  "Serve skkjure server"
+  [& _args]
+  (let [system (-> (c.core/new-system)
+                   component/start-system)]
+    (-> (Runtime/getRuntime)
+        (.addShutdownHook (Thread. (fn []
+                                     (log/info "Shutdown signal received")
+                                     (component/stop-system system)
+                                     (log/info "Shutdown hook completed")))))
+    (loop [_ nil]
+      (recur (Thread/sleep Long/MAX_VALUE)))))
+
+(defn cmd-help
+  "Help"
+  [& _args]
+  (eprintln "Available commands:")
+  (doseq [[k v] commands]
+    (-> (format "  %s - %s" (name k) (:doc (meta v)))
+        eprintln)))
+
+(def commands {:serve #'cmd-serve
+               :help #'cmd-help})
+
 (defn -main [& args]
-  (println "Hello world"))
+  (if-let [command (get commands (keyword (first args)))]
+    (apply command args)
+    (do
+      (when (first args)
+        (eprintln (format "`%s' is not a recognized command." (first args))))
+      (apply cmd-help args))))
