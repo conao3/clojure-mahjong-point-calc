3900be0f5002074e725ecdf50878dcf4b282cb0d
conao3 <conao3@gmail.com>
2025-08-03 17:38:51 +0900

    malli config

--- dev/dev.clj
+++ dev/dev.clj
@@ -4,9 +4,21 @@
    #_{:clj-kondo/ignore [:unused-referred-var]}
    [com.stuartsierra.component.repl :as component.repl :refer [system start stop reset initializer]]
    [mahjong-point-calc.system :as c.system]
-   [malli.dev :as malli.dev]))
+   [malli.dev :as malli.dev]
+   [malli.dev.pretty :as malli.dev.pretty]))
 
 (apply require clojure.main/repl-requires)
 
 (component.repl/set-init (fn [_] (c.system/new-system)))
-(malli.dev/start!)
+(malli.dev/start! {:report (malli.dev.pretty/reporter
+                            (malli.dev.pretty/-printer
+                             {:colors
+                              ;; default: malli.dev.virhe.-dark-colors
+                              {:title 33
+                               :title-dark 28
+                               :text 238
+                               :link 21
+                               :string 136
+                               :constant 91
+                               :type 30
+                               :error 160}}))})
