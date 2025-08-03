ab80ef184702de770d1644a222f679a95c41f7c3
conao3 <conao3@gmail.com>
2025-08-04 00:30:42 +0900

    remove test.clj

--- dev/test.clj
+++
@@ -1,8 +0,0 @@
-(ns test
-  (:require
-   [malli.dev :as malli.dev]
-   [malli.dev.pretty :as malli.dev.pretty]))
-
-(set! *warn-on-reflection* true)
-
-(malli.dev/start! {:report (malli.dev.pretty/thrower)})
